4a6cdb26a5589ce994d066d141390ab54dff7f21
David Volm <david@volminator.com>
2021-10-09 13:07:46 -0500

    Fix: The value of the “for” attribute of the “label” element must be the ID of a non-hidden form control.

--- index.php
+++ index.php
@@ -12,25 +12,25 @@ print "<form action=\"./another-page.php\" method=\"POST\">\n";
 # Username:
 print "\t<p>\n";
 print "\t\t<label for=\"username\">Username:</label><br>\n";
-print "\t\t<input type=\"text\" name=\"username\"><br>\n";
+print "\t\t<input type=\"text\" id=\"username\" name=\"username\"><br>\n";
 print "\t</p>\n";
 
 # Password:
 print "\t<p>\n";
 print "\t\t<label for=\"password\">Password:</label><br>\n";
-print "\t\t<input type=\"password\" name=\"password\"><br>\n";
+print "\t\t<input type=\"password\" id=\"password\" name=\"password\"><br>\n";
 print "\t</p>\n";
 
 # Confirm password:
 print "\t<p>\n";
 print "\t\t<label for=\"confirm-password\">Confirm password:</label><br>\n";
-print "\t\t<input type=\"password\" name=\"confirm-password\"><br>\n";
+print "\t\t<input type=\"password\" id=\"confirm-password\" name=\"confirm-password\"><br>\n";
 print "\t</p>\n";
 
 # Email address:
 print "\t<p>\n";
 print "\t\t<label for=\"email-address\">Email address:</label><br>\n";
-print "\t\t<input type=\"text\" name=\"email-address\"><br>\n";
+print "\t\t<input type=\"text\" id=\"email-address\" name=\"email-address\"><br>\n";
 print "\t</p>\n";
 
 print "\t<button type=\"submit\" value=\"register\">Register</button> <br>\n";
